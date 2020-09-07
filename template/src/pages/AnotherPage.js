54caed83a6da87767e025392e22bf226f020cd6f
davidaxel121 <davidaxel121@gmail.com>
2020-09-06 20:03:00 -0500

    Adding navbar with styles

--- template/src/pages/AnotherPage.js
+++ template/src/pages/AnotherPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import MyNavbar from '../components/Navbar';
 
 import './styles/AnotherPage.css';
 
 const AnotherPage = () => {
 	return (
 		<div className="container-fluid another-page-container">
+<div className="row">
+				<div className="col-12">
+					<MyNavbar />
+				</div>
+			</div>
 
 			<div className="row d-flex align-items-center justify-content-center another-page-hero">
 				<div className="col-12 col-md-6 text-center">

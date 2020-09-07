e1b609f4af8608e113d855772678a5b26e27a567
davidaxel121 <davidaxel121@gmail.com>
2020-09-06 19:39:43 -0500

    Adding react bootstrap configurations

--- template/src/pages/AnotherPage.js
+++ template/src/pages/AnotherPage.js
@@ -4,11 +4,15 @@ import './styles/AnotherPage.css';
 
 const AnotherPage = () => {
 	return (
-		<>
+		<div className="container-fluid another-page-container">
 
-			<h1>Another Page</h1>
+			<div className="row d-flex align-items-center justify-content-center another-page-hero">
+				<div className="col-12 col-md-6 text-center">
+					<h1 className="another-page-title">Another page</h1>
+				</div>
+			</div>
 
-		</>
+		</div>
 	)
 }
 

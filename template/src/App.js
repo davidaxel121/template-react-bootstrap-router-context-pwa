1e58424a9bd1f2d123d9ef15b40b11392265fe9d
davidaxel121 <davidaxel121@gmail.com>
2020-09-06 19:18:03 -0500

    Adding react router dom configurations

--- template/src/App.js
+++ template/src/App.js
@@ -1,26 +1,40 @@
+// import React, { useState, useMemo } from "react";
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import AnotherPage from './pages/AnotherPage';
+
 import './App.css';
 
+// import { Context } from "./store/GlobalContext";
+
+/**
+ * Default App with Global Context and Router
+ *
+ * @version 1.0.0
+ * @author [DA]
+ */
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	// const [globalState, setGlobalState] = useState({
+	// 	userProfile: null, 
+	// 	userSession: false
+	// });
+
+	// const value = useMemo(() => ({ globalState, setGlobalState }), [globalState, setGlobalState]);
+
+	return (
+		<BrowserRouter>
+			<Switch>
+				{/* <Context.Provider value={value}> */}
+				<Route exact path="/" component={Home} />
+				<Route exact path="/anotherpage" component={AnotherPage} />
+				{/* </Context.Provider> */}
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
